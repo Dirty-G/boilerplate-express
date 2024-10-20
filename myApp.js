ebd67143434cf20869fe5def2fd4b37390deca05
Curt Gert <gertsch3@yahoo.com>
2024-10-20 23:53:33 +0000

    Send Hello

--- myApp.js
+++ myApp.js
@@ -2,7 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-
+// send hello express
+app.get("/", function(req,res){
+    res.send("Hello Express");
+  })
 
 
 
